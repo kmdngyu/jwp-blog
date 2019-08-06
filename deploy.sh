baccc2cbdc02fca1af5e5dceb75960cb1fd69432
kmdngyu <kmdngyu@gmail.com>
2019-08-06 16:54:30 +0900

    fix: 배포 구문

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ REPOSITORY=/home/ubuntu/app
 git pull
 CURRENT_PID=$(pgrep -f MyblogApplication)
 if [ -z $CURRENT_PID ]; then
+	echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
         kill -9 $CURRENT_PID
         sleep 5
